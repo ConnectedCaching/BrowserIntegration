f5a3386793f677562059b9d3fb87ca08f5f410e5
Leo Schweizer <leonhard.schweizer@gmail.com>
2013-10-13 23:12:30 +0200

    refactoring of extractor matching

--- chrome/background.js
+++ chrome/background.js
@@ -1,34 +1,39 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 	if (!changeInfo.status) return;
-
-	console.log(changeInfo.status + ' ' + tab.url);
 	chrome.pageAction.hide(tabId);
-
 	if (changeInfo.status != 'complete') return;
 
-	// opencaching.com geocache detail page
-	if (tab.url.match(/http\:\/\/www\.opencaching\.com\/.*\/#!geocache\/.*/i)) {
-		chrome.pageAction.show(tabId);
-		chrome.tabs.executeScript(tabId, {file: "extractors/opencaching.com.detail.js"});
-	}
+	var extractors = [
+		// opencaching.com geocache detail page
+		{
+			match: [/http\:\/\/www\.opencaching\.com\/.*\/#!geocache\/.*/i],
+			extractor: "extractors/opencaching.com.detail.js"
+		},
+		// opencaching.com map view
+		{
+			match: [/http:\/\/www\.opencaching\.com\/.*\/#find.*/i],
+			extractor: "extractors/opencaching.com.map.js"
+		},
+		// geocaching.com detail page
+		{
+			match: [
+				/http:\/\/www\.geocaching\.com\/geocache\/.*/i,
+				/http:\/\/www\.geocaching\.com\/seek\/cache_details\.aspx.*/i
+			],
+			extractor: "extractors/geocaching.com.detail.js"
+		}
+	];
 
-	// opencaching.com map view
-	if (tab.url.match(/http:\/\/www\.opencaching\.com\/.*\/#find.*/i)) {
-		chrome.pageAction.show(tabId);
-		chrome.tabs.executeScript(tabId, {file: "extractors/opencaching.com.map.js"});
-	}
-
-	// geocaching.com detail page
-	if (tab.url.match(/http:\/\/www\.geocaching\.com\/geocache\/.*/i)) {
-		chrome.pageAction.show(tabId);
-		chrome.tabs.executeScript(tabId, {file: "extractors/geocaching.com.detail.js"});
-	}
-	
-	// geocaching.com detail page (alternative url)
-	if (tab.url.match(/http:\/\/www\.geocaching\.com\/seek\/cache_details\.aspx.*/i)) {
-		chrome.pageAction.show(tabId);
-		chrome.tabs.executeScript(tabId, {file: "extractors/geocaching.com.detail.js"});
-	}
+	extractors.some(function(matchDef) {
+		return matchDef.match.some(function(match) {
+			if (match.test(tab.url)) {
+				chrome.pageAction.show(tabId);
+				chrome.tabs.executeScript(tabId, {file: matchDef.extractor});
+				return true;
+			}
+			return false;
+		});
+	});
 
 });
