edd511901b0c6795ab1b831409e85a1d5274dff7
Leo Schweizer <leonhard.schweizer@gmail.com>
2013-10-13 15:47:58 +0200

    auth detection

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,5 +1,16 @@
 
+function displayLoginWarning() {
+	$('main').prepend('<section class="card"><h1><strong class="warning">Sign In Required</strong></h1><h2>' + 
+		'Please <a href="#" class="warning">sign in</a> to your Connected Caching account!</h2></section>');
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+	// check if user is signed in to Connected Caching
+	chrome.cookies.get({url: 'http://localhost/*', name: 'authToken'}, function(cookie) {
+		console.dir(cookie);
+		if (cookie == null) return displayLoginWarning();
+	});
+
 	// query current tab's extractor for displayed geocaches
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {type: "extract_preview"}, function(result) {
